993496f008487ecbaeeda3a7ad552ccccdb4d398
Graeme Meyer <graememeyer@hotmail.com>
2020-05-01 08:25:35 +0100

    commit

--- Install-Splunk.ps1
+++ Install-Splunk.ps1
@@ -1,9 +1,9 @@
 # TODO elevate to administrator if necessary
 
 # Get the latest version number of Splunk
-$VersionNuumber = "8.0.3" # Set default version 2020-04-30
-$FileName = splunk-8.0.3-a6754d8441bf-x64-release.msi # Set default FileName 2020-04-30
-$Site = Invoke-WebRequest https://www.splunk.com/en_us/download/sem.html
+$VersionNumber = "8.0.3" # Set default version 2020-04-30
+$FileName = "splunk-8.0.3-a6754d8441bf-x64-release.msi" # Set default FileName 2020-04-30
+$Site = Invoke-WebRequest "https://www.splunk.com/en_us/download/sem.html"
 foreach($Link in $Site.links)
 {
     if($Link.innerHTML -match "splunk-(?<version>\d+\.\d+\.\d+)-\w+-x64-release.msi")
@@ -20,7 +20,9 @@ foreach($Link in $Site.links)
 $BaseDownloadUrl = "https://download.splunk.com/products/splunk/releases/"
 $Platform = "windows"
 $DownloadUrl = $BaseDownloadUrl + $VersionNumber + "/" + $Platform + "/" + $FileName
- 
-Write-Host "$($DownloadUrl)"
+$OutPath = "$($env:USERPROFILE)\Desktop\$($FileName)"
 
-Invoke-WebRequest $DownloadUrl | Out-File $env:USERPROFILE\Desktop\$FileName
+Write-Host "Downloading from: $($DownloadUrl)"
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest $DownloadUrl -OutFile $OutPath
+Write-Host "Wrote file to $($OutPath)"
--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 PowerShell script to download and install Splunk Enterprise 
 
 Release Goals:
-* Detect the latest version of Splunk
-* Download the latest version of Splunk
+* Detect the latest version of Splunk ✔
+* Download the latest version of Splunk ✔
+* Time the download and output the results
 * Install with default settings
 * No user interaction required
 * Automatically create required accounts and return credentials to the user
@@ -15,6 +16,7 @@ Stretch Goals:
 * Elevate to admin if required
 * Download progress indicator
 * Install progress indicator
-* Multiple download strategies
+* Multiple download strategies, defaulting to the fastest
 * Configurable options
 * Automatically open Splunk console in the default browser
+* Automatically update the git repository with the latest-known version numbers
